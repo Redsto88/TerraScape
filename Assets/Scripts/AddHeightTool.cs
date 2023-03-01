776487a0dcbab92245fdc6ec0d4aa90385025cd2
Ugo-Larsonneur <ugo.larsonneur0@gmail.com>
2023-03-01 18:08:37 +0100

    Fix

--- Assets/Scripts/AddHeightTool.cs
+++ Assets/Scripts/AddHeightTool.cs
@@ -9,7 +9,7 @@ public class AddHeightTool : TerrainTool
     [SerializeField] float radius = 3f;
     [SerializeField] AnimationCurve fallOff;//TODO: use
 
-    public override void ApplyTool(Vector3 pos, Vector3 normal, Terrain terrain)
+    public void ApplyTool2(Vector3 pos, Vector3 normal, Terrain terrain)
     {
         TerrainData terrainData = terrain.terrainData;
         int resolution = terrainData.heightmapResolution;
@@ -39,5 +39,46 @@ public class AddHeightTool : TerrainTool
         }
 
         terrainData.SetHeights(minX, minY, newHeights);
+    }
+
+    public override void ApplyTool(Vector3 pos, Vector3 normal, Terrain terrain)
+    {
+        TerrainData terrainData = terrain.terrainData;
+        int resolution = terrainData.heightmapResolution;
+
+        Vector3 objectCenter = (pos - terrain.gameObject.transform.position);
+        Vector2 indexSpaceCenter = new Vector2(objectCenter.x / terrainData.size.x, objectCenter.z / terrainData.size.z) * (resolution - 1);
+        Vector2 indexSpaceRadius = new Vector2(radius / terrainData.size.x, radius / terrainData.size.z) * (resolution - 1);
+
+        int minX = Mathf.RoundToInt(indexSpaceCenter.x - indexSpaceRadius.x);
+        int maxX = Mathf.RoundToInt(indexSpaceCenter.x + indexSpaceRadius.x);
+        int minY = Mathf.RoundToInt(indexSpaceCenter.y - indexSpaceRadius.y);
+        int maxY = Mathf.RoundToInt(indexSpaceCenter.y + indexSpaceRadius.y);
+        int clampedMinX = Mathf.Clamp(minX, 0, resolution - 1);
+        int clampedMaxX = Mathf.Clamp(maxX, 0, resolution - 1);
+        int clampedMinY = Mathf.Clamp(minY, 0, resolution - 1);
+        int clampedMaxY = Mathf.Clamp(maxY, 0, resolution - 1);
+
+        if(clampedMinX > clampedMaxX || clampedMinY > clampedMaxY)
+            throw new System.Exception("Zone incorrecte");
+
+        float[,] newHeights = terrainData.GetHeights(clampedMinX, clampedMinY, clampedMaxX - clampedMinX, clampedMaxY - clampedMinY);
+
+        for (int y = clampedMinY ; y < clampedMaxY; y++)
+        {
+            float brushY = (float)(y - minY) / (maxY - minY - 1);
+
+            for (int x = clampedMinX; x < clampedMaxX; x++)
+            {
+                float brushX = (float)(x - minX) / (maxX - minX - 1);
+
+                //float curveUV = Mathf.Clamp01(new Vector2(1f - Mathf.Abs(brushX - 0.5f) * 2f, 1f - Mathf.Abs(brushY - 0.5f) * 2f).magnitude);
+                float curveUV = 1f - new Vector2(Mathf.Abs(brushX - 0.5f) * 2f, Mathf.Abs(brushY - 0.5f) * 2f).magnitude;
+                //TODO: SetHeightsDelayLOD
+                newHeights[y - clampedMinY, x - clampedMinX] += (height / terrainData.size.y) * Time.deltaTime * fallOff.Evaluate(curveUV);
+            }
+        }
+
+        terrainData.SetHeights(clampedMinX, clampedMinY, newHeights);
     }
 }
