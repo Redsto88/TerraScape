1f7b685b95ee4b1f620e2b007eb46a6e3c6d8e8f
Ugo-Larsonneur <ugo.larsonneur0@gmail.com>
2023-03-10 10:46:06 +0100

    fix

--- Assets/Scripts/ChangeTextureTool.cs
+++ Assets/Scripts/ChangeTextureTool.cs
@@ -26,7 +26,7 @@ public class ChangeTextureTool : TerrainTool
         textureNumber = newTexture;
     }
 
-    public override void Apply(Vector3 pos, Vector3 normal, Terrain terrain, float multiplier=1f)
+    public override void Apply(Vector3 pos, Vector3 normal, Terrain terrain)
     {
         TerrainData terrainData = terrain.terrainData;
         int resolution = terrainData.alphamapResolution;
