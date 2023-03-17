6e61e0620fc4306df44758a86c273b6fa5d2b161
Ugo-Larsonneur <ugo.larsonneur0@gmail.com>
2023-03-17 11:07:00 +0100

    reticle fix

--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -8,7 +8,7 @@ public class Reticle : MonoBehaviour
 
     public virtual void SetSize(float size)
     {
-        anchor.localScale = new Vector3(size, size, size);
+        anchor.localScale = new Vector3(2f*size, anchor.localScale.y, 2f*size);
     }
 
     public virtual void OnHover(Vector3 pos, Vector3 normal)
