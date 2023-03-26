91677db81b5af9e623b807aa4914c5f4f920bc17
Ugo-Larsonneur <ugo.larsonneur0@gmail.com>
2023-03-26 13:35:10 +0200

    better checkmark pos

--- Assets/Scripts/Checkmark.cs
+++ Assets/Scripts/Checkmark.cs
@@ -6,6 +6,6 @@ public class Checkmark : MonoBehaviour
 {
     public void Move(RectTransform target)
     {
-        GetComponent<RectTransform>().localPosition = target.localPosition /*+ new Vector3(10, 0, 0)*/;
+        GetComponent<RectTransform>().localPosition = target.localPosition + new Vector3(-17, 0, 0);
     }
 }
